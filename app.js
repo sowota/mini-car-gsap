82550f09bda9971cdcb9a2d401bc183ba1c8d108
Yuri <70051216+sowota@users.noreply.github.com>
2021-10-11 15:57:01 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+gsap.to("#red-car", {
+    duration:6,
+    x:1960,
+    y:439,
+    ease:"none",
+    repeat:4,
+    repeatDelay:1
+})
+
+gsap.to("#blue-car", {
+    duration:12,
+    x:570,
+    y:100,
+    ease:"none",
+    repeat:4,
+    repeatDelay:2.5
+})
+gsap.to("#yellow-car", {
+    duration:7,
+    x:884,
+    y:1114,
+    ease:"none",
+    repeat:4,
+    repeatDelay:1
+})
